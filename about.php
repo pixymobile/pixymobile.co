cceae1d830242db11d7eb63c898b148ef245f4bd
Berat ŞEN <beratsen@nngrup.com.tr>
2021-09-16 16:59:45 +0300

    bazi sayfalar
    bazi sayfalar

---
+++ about.php
@@ -0,0 +1,318 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+   
+<head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>Pixy - Hakkımızda </title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Place favicon.ico in the root directory -->
+      <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+      <!-- CSS here -->
+      <link rel="stylesheet" href="assets/css/preloader.css">
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+      <link rel="stylesheet" href="assets/css/meanmenu.css">
+      <link rel="stylesheet" href="assets/css/animate.min.css">
+      <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+      <link rel="stylesheet" href="assets/css/backToTop.css">
+      <link rel="stylesheet" href="assets/css/jquery.fancybox.min.css">
+      <link rel="stylesheet" href="assets/css/fontAwesome5Pro.css">
+      <link rel="stylesheet" href="assets/css/elegantFont.css">
+      <link rel="stylesheet" href="assets/css/default.css">
+      <link rel="stylesheet" href="assets/css/style.css">
+   </head>
+   <body>
+      <!--[if lte IE 9]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+      <![endif]-->
+      
+      <!-- Add your site or application content here -->  
+
+      <!-- pre loader area start -->
+      <div id="loading">
+         <div id="loading-center">
+            <div id="loading-center-absolute">
+               <div class="object" id="object_one"></div>
+               <div class="object" id="object_two" style="left:20px;"></div>
+               <div class="object" id="object_three" style="left:40px;"></div>
+               <div class="object" id="object_four" style="left:60px;"></div>
+               <div class="object" id="object_five" style="left:80px;"></div>
+            </div>
+         </div>  
+      </div>
+      <!-- pre loader area end -->
+
+      <!-- back to top start -->
+      <div class="progress-wrap">
+         <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+         </svg>
+      </div>
+      <!-- back to top end -->
+
+      <!-- header area start -->
+      <?php
+               $foo = include 'header.php';
+         
+      ?>
+    
+      <!-- header area end -->
+
+      <!-- sidebar area start -->
+      <div class="sidebar__area">
+         <div class="sidebar__wrapper">
+            <div class="sidebar__close">
+               <button class="sidebar__close-btn" id="sidebar__close-btn">
+               <span><i class="fal fa-times"></i></span>
+               <span>close</span>
+               </button>
+            </div>
+            <div class="sidebar__content">
+               <div class="logo mb-40">
+                  <a href="index.html">
+                  <img src="assets/img/logo/logo.png" alt="logo">
+                  </a>
+               </div>
+               <div class="mobile-menu mobile-menu-2"></div>
+               <div class="sidebar__info mt-350">
+                  <a href="#" class="w-btn w-btn-blue-2 w-btn-4 d-block mb-15 mt-15">login</a>
+                  <a href="#" class="w-btn w-btn-blue d-block">sign up</a>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- sidebar area end -->      
+      <div class="body-overlay"></div>
+      <!-- sidebar area end -->
+
+      <main>
+
+        <!-- page title area start -->
+        <section class="page__title-area page__title-height d-flex align-items-center fix p-relative z-index-1" data-background="assets/img/page-title/page-title.jpg">
+         <div class="page__title-shape">
+            <img class="page-title-dot-4" src="assets/img/page-title/dot-4.png" alt="">
+            <img class="page-title-dot" src="assets/img/page-title/dot.png" alt="">
+            <img class="page-title-dot-2" src="assets/img/page-title/dot-2.png" alt="">
+            <img class="page-title-dot-3" src="assets/img/page-title/dot-3.png" alt="">
+            <img class="page-title-plus" src="assets/img/page-title/plus.png" alt="">
+            <img class="page-title-triangle" src="assets/img/page-title/triangle.png" alt="">
+         </div>
+           <div class="container">
+              <div class="row">
+                 <div class="col-xxl-12">
+                    <div class="page__title-wrapper text-center">
+                       <h3>Hakkımızda </h3>
+                        <nav aria-label="breadcrumb">
+                           <ol class="breadcrumb justify-content-center">
+                              <li class="breadcrumb-item"><a href="index.html">Anasayfa</a></li>
+                              <li class="breadcrumb-item active" aria-current="page">Hakkımızda</li>
+                           </ol>
+                        </nav>
+                    </div>
+                 </div>
+              </div>
+           </div>
+        </section>
+        <!-- page title area end -->
+
+         <!-- services area start -->
+         <section class="services__area pt-110 pb-45">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xxl-6 offset-xl-3 col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 col-md-10 offset-md-1">
+                     <div class="section__title-wrapper section__title-wrapper-4 text-center mb-65 wow fadeInUp" data-wow-delay=".3s">
+                        <h2 class="section__title section__title-4 section__title-4-p-2">Profosyonel Uygulamalar Yazıyoruz</h2>
+                        <p>Uygulamalarımızda son trendleri takip ediyoruz, planlıyoruz, çiziyoruz, kodluyoruz, pazarlıyoruz! İşte başarımızın sırrı burada.</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay=".3s">
+                     <div class="services__item-4 white-bg mb-30">
+                        <div class="services__thumb-4 text-center d-flex align-items-end justify-content-center">
+                           <img src="assets/img/services/home-4/services-1.png" alt="">
+                        </div>
+                        <div class="services__content-4">
+                           <h3 class="services__title-4"><a href="services-details.html">Kullanıcılarımızın mutluluğuna önem veriyoruz.</a></h3>
+                           <p>Klasik bir tasarım yerine minimal çizgiler kullanarak uygulamayı sıradışı haline getiriyoruz.</p>
+                           
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay=".5s">
+                     <div class="services__item-4 white-bg mb-30">
+                        <div class="services__thumb-4 text-center d-flex align-items-end justify-content-center">
+                           <img src="assets/img/services/home-4/services-2.png" alt="">
+                        </div>
+                        <div class="services__content-4">
+                           <h3 class="services__title-4"><a href="services-details.html">Start workflows from any app</a></h3>
+                           <p>I smashing, hanky panky chap plastered show off show off pick your nose plastered.</p>
+                           <a href="services-details.html" class="link-btn">Chat Platfrom <i class="arrow_right"></i> </a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+         <!-- services area end -->
+
+         <!-- about area start -->
+         <section class="about__area pb-45 pt-45 p-relative">
+            <div class="container">
+               <div class="row align-items-center">
+                  <div class="col-xxl-5 col-xl-6 col-lg-6 col-md-9 order-last order-lg-first">
+                     <div class="about__wrapper about__wrapper-2 mb-20">
+                        <div class="section__title-wrapper mb-25 wow fadeInUp" data-wow-delay=".3s">
+                           <h2 class="section__title">Awesome Prototyping Tool for UI/UX.</h2>
+                           <p>Starkers pardon you knees up is Elizabeth geeza Why, quain standard  guvnor gosh cras brilliant.</p>
+                        </div>
+                        <ul class="wow fadeInUp" data-wow-delay=".5s">
+                           <li>Intergate With Popular Softwares item</li>
+                           <li>Instantly Create Your Crowdfunding Platform</li>
+                        </ul>
+                        <a href="contact.html" class="w-btn w-btn-3 w-btn-1">Get Started</a>
+                     </div>
+                  </div>
+                  <div class="col-xxl-6 offset-xxl-1 col-xl-6 col-lg-6 wow fadeInRight" data-wow-delay=".7s">
+                     <div class="about__thumb-wrapper-2 ml-40 p-relative m-img">
+                        <img src="assets/img/about/about-img.jpg" alt="">
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+         <!-- about area end -->
+
+         <!-- faq area start -->
+         <section class="faq__area pt-75 pb-120">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12">
+                     <div class="faq__wrapper wow fadeInUp" data-wow-delay=".3s">
+                        <div class="accordion" id="accordionExample">
+                           <div class="accordion-item">
+                              <h2 class="accordion-header" id="headingOne">
+                                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                 What to do if you can’t access ?
+                                 </button>
+                              </h2>
+                              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                 <div class="accordion-body">
+                                 <p>Looking to start an online store and you're not sure where to begin? I'll guide will help you to navigate Would you like to boost your Twitter profile.</p>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="accordion-item">
+                              <h2 class="accordion-header" id="headingTwo">
+                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                 How to start an online store in 2021 ?
+                                 </button>
+                              </h2>
+                              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                 <div class="accordion-body">
+                                 <p>Looking to start an online store and you're not sure where to begin? I'll guide will help you to navigate Would you like to boost your Twitter profile.</p>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="accordion-item border-none">
+                              <h2 class="accordion-header" id="headingThree">
+                                 <button class="accordion-button collapsed pb-0 " type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                 How to change font size in wordPress ?
+                                 </button>
+                              </h2>
+                              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                 <div class="accordion-body">
+                                 <p>Looking to start an online store and you're not sure where to begin? I'll guide will help you to navigate Would you like to boost your Twitter profile.</p>
+                                 </div>
+                              </div>
+                           </div>
+                           </div>
+                     </div>
+                  </div>
+                  <div class="col-xxl-5 offset-xxl-1 col-xl-5 offset-xl-1 col-lg-6 col-md-8">
+                     <div class="faq__content">
+                        <div class="section__title-wrapper section__title-wrapper-2 mb-35 wow fadeInUp" data-wow-delay=".3s">
+                           <h2 class="section__title section__title-2">Loved and trusted by over 40k+ users!</h2>
+                           <p>Over the last few years, podcasts have become a huge deal. They’ve taken on a growing role.</p>
+                        </div>
+                        <div class="faq__counter wow fadeInUp" data-wow-delay=".5s">
+                           <ul>
+                              <li>
+                                 <h3 class="pink"><span class="counter">876</span></h3>
+                                 <p>Happy Clients</p>
+                              </li>
+                              <li>
+                                 <h3 class="blue"><span class="counter">156</span></h3>
+                                 <p>Projects</p>
+                              </li>
+                              <li>
+                                 <h3 class="yellow"><span class="counter">430</span></h3>
+                                 <p>Trusted Users</p>
+                              </li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+         <!-- faq area end -->
+
+         <!-- promotion area start -->
+         <section class="promotion__area promotion__bg pt-125 pb-125 p-relative">
+            <div class="promotion__shape">
+               <img class="promotion-dot" src="assets/img/icon/promotion/pro-dot.png" alt="">
+               <img class="promotion-plus" src="assets/img/icon/promotion/pro-plus.png" alt="">
+               <img class="promotion-triangle" src="assets/img/icon/promotion/pro-triangle.png" alt="">
+            </div>
+            <div class="container">
+               <div class="row">
+                  <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                     <div class="promotion__content wow fadeInUp" data-wow-delay=".3s">
+                        <h3 class="promotion__title">Product goals? <br> Let's make them happen.</h3>
+                        <p>They’ve taken on a growing role Over the last few year.</p>
+                        <a href="about.html" class="w-btn w-btn-8">read more</a>
+                     </div>
+                  </div>
+                  <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
+                     <div class="promotion__content pl-70 promotion__right wow fadeInUp" data-wow-delay=".5s">
+                        <h3 class="promotion__title">Looking for enterprise <br> solution.</h3>
+                        <p>They’ve taken on a growing role Over the last few year.</p>
+                        <a href="about.html" class="w-btn w-btn-8">read more</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+         <!-- promotion area end -->
+      </main>
+
+      <!-- footer area start -->
+       <?php
+               $foo = include 'footer.php';
+         
+      ?>
+    
+      <!-- footer area end -->
+
+      <!-- JS here -->
+      <script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
+      <script src="assets/js/vendor/waypoints.min.js"></script>
+      <script src="assets/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/js/jquery.meanmenu.js"></script>
+      <script src="assets/js/owl.carousel.min.js"></script>
+      <script src="assets/js/jquery.fancybox.min.js"></script>
+      <script src="assets/js/isotope.pkgd.min.js"></script>
+      <script src="assets/js/parallax.min.js"></script>
+      <script src="assets/js/backToTop.js"></script>
+      <script src="assets/js/jquery.counterup.min.js"></script>
+      <script src="assets/js/ajax-form.js"></script>
+      <script src="assets/js/wow.min.js"></script>
+      <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+      <script src="assets/js/main.js"></script>
+   </body>
+
+<!-- Mirrored from themepure.net/template/wetland-prev/wetland/about.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 09 Sep 2021 07:24:43 GMT -->
+</html>
+
--- contact.php
+++ contact.php
@@ -53,7 +53,7 @@
 
       <!-- header area start -->
        <?php
-               $foo = include 'header_v1.php';
+               $foo = include 'header.php';
       
      
          ?>
@@ -125,7 +125,7 @@
                          <div class="row">
                             <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
                                <div class="contact__info pr-80">
-                                  <h3 class="contact__title">İletişimde kalın</h3>
+                                  <h3 class="contact__title">İletişime geçin</h3>
       
                                   <div class="contact__details">
                                      <ul>
@@ -235,112 +235,11 @@
       </main>
 
       <!-- footer area start -->
-      <footer class="footer__area grey-bg-3 pt-120 p-relative fix">
-         <div class="footer__shape">
-            <img class="footer-circle-1 footer-2-circle-1" src="assets/img/icon/footer/home-1/circle-1.png" alt="">
-            <img class="footer-circle-2 footer-2-circle-2" src="assets/img/icon/footer/home-1/circle-2.png" alt="">
-         </div>
-         <div class="footer__top pb-65">
-            <div class="container">
-               <div class="row">
-                  <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6 wow fadeInUp" data-wow-delay=".3s">
-                     <div class="footer__widget mb-50">
-                        <div class="footer__widget-title mb-25">
-                           <div class="footer__logo">
-                              <a href="#">
-                                 <img src="assets/img/logo/logo.png" alt="logo">
-                              </a>
-                           </div>
-                        </div>
-                        <div class="footer__widget-content">
-                           <p>Ever since we started using  we’ve , and more.</p>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-4 col-sm-6 wow fadeInUp" data-wow-delay=".5s">
-                     <div class="footer__widget mb-50 footer__pl-70">
-                        <div class="footer__widget-title mb-25">
-                           <h3>Overview</h3>
-                        </div>
-                        <div class="footer__widget-content">
-                           <div class="footer__link footer__link-2">
-                              <ul>
-                                 <li><a href="#">Terms</a></li>
-                                 <li><a href="#">Privacy Policy</a></li>
-                                 <li><a href="#">Cookies</a></li>
-                                 <li><a href="#">Integrations</a></li>
-                              </ul>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-xxl-3 col-xl-2 col-lg-2 col-md-4 col-sm-6 wow fadeInUp" data-wow-delay=".7s">
-                     <div class="footer__widget mb-50 footer__pl-90">
-                        <div class="footer__widget-title mb-25">
-                           <h3>Customer</h3>
-                        </div>
-                        <div class="footer__widget-content">
-                           <div class="footer__link footer__link-2">
-                              <ul>
-                                 <li><a href="#">Home</a></li>
-                                 <li><a href="#">Product</a></li>
-                                 <li><a href="#">Pricing</a></li>
-                                 <li><a href="#">Integrations</a></li>
-                              </ul>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-4 col-sm-6 wow fadeInUp" data-wow-delay=".9s">
-                     <div class="footer__widget mb-50">
-                        <div class="footer__widget-title mb-25">
-                           <h3>Product</h3>
-                        </div>
-                        <div class="footer__widget-content">
-                           <div class="footer__link footer__link-2">
-                              <ul>
-                                 <li><a href="#">Getting Started</a></li>
-                                 <li><a href="#">Style Guide</a></li>
-                                 <li><a href="#">Licences</a></li>
-                                 <li><a href="#">Changelog</a></li>
-                              </ul>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-xxl-2 col-xl-3 col-lg-3 col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="1.2s">
-                     <div class="footer__widget mb-50 float-md-end fix">
-                        <div class="footer__widget-title mb-25">
-                           <h3>Follow Us</h3>
-                        </div>
-                        <div class="footer__widget-content">
-                           <div class="footer__social footer__social-2">
-                              <ul>
-                                 <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                 <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                 <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
-                              </ul>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-         <div class="footer__bottom">
-            <div class="container">
-               <div class="footer__copyright">
-                  <div class="row">
-                     <div class="col-xxl-12 wow fadeInUp" data-wow-delay=".5s">
-                        <div class="footer__copyright-wrapper footer__copyright-wrapper-2 text-center">
-                           <p>Copyright © 2021 All Rights Reserved passion by <a href="#">Pixy Mobile</a></p>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </footer>
+        <?php
+               $foo = include 'footer.php';
+      
+     
+         ?>
       <!-- footer area end -->
 
       <!-- JS here -->
